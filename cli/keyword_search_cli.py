9a73f109f8f6be35d3cd4b50c786d5bf8a36e3b3
Greg Delgado <grd888@gmail.com>
2026-01-12 15:40:49 +0800

    Build an Inverted Index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,8 +2,7 @@
 
 import argparse
 
-from lib.keyword_search import search_command
-
+from lib.keyword_search import search_command, build_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -11,6 +10,8 @@ def main() -> None:
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+
+    subparsers.add_parser("build", help="Build the inverted index")
 
     args = parser.parse_args()
 
@@ -20,6 +21,10 @@ def main() -> None:
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
                 print(f"{i}. {res['title']}")
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,15 +1,27 @@
 import string
+import os
+import pickle
 from nltk.stem import PorterStemmer
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stop_words
+from .search_utils import DEFAULT_SEARCH_LIMIT, CACHE_DIR, load_movies, load_stop_words
 
 _STOP_WORDS = None
 _STEMMER = PorterStemmer()
+
 
 def _get_stop_words() -> list[str]:
     global _STOP_WORDS
     if _STOP_WORDS is None:
         _STOP_WORDS = load_stop_words()
     return _STOP_WORDS
+
+
+def build_command() -> None:
+    inverted_index = InvertedIndex()
+    inverted_index.build()
+    inverted_index.save()
+    docs = inverted_index.get_documents("merida")
+    print(f"First document for token 'merida' = {docs[0]}")
+
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
@@ -48,3 +60,50 @@ def tokenize_text(text: str) -> list[str]:
         _STEMMER.stem(token) for token in tokens if token and token not in stop_words
     ]
     return valid_tokens
+
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        # a mapping of tokens to document IDs
+        self.index: dict[str, set[int]] = {}
+        # a mapping of document IDs to their full document objects
+        self.doc_map: dict[int, dict] = {}
+
+        self.index_path = os.path.join(CACHE_DIR, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
+
+    def _add_document(self, doc_id: int, text: str):
+        # tokenize the document text
+        tokens = tokenize_text(text)
+        # add each token to the index with the document ID
+        for token in tokens:
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term: str) -> list[int]:
+        """
+        Get the set of document IDs for a given term, and return them as a list,
+        sorted in ascending order. (Assume input is a single word/token)
+        """
+        return sorted(list(self.index.get(term, set())))
+
+    def build(self):
+        """
+        Build the inverted index from the documents in the dataset.
+        """
+        movies = load_movies()
+        for movie in movies:
+            # concatenate the title and description
+            self._add_document(movie["id"], f"{movie['title']} {movie['description']}")
+            self.doc_map[movie["id"]] = movie
+
+    def save(self):
+        """
+        Save the inverted index to a file.
+        """
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        with open(self.index_path, "wb") as f:
+            pickle.dump(self.index, f)
+        with open(self.docmap_path, "wb") as f:
+            pickle.dump(self.doc_map, f)
