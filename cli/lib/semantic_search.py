9d4025b970e92bfc831d33581535dec4ab4b92df
Greg Delgado <grd888@gmail.com>
2026-01-13 20:28:17 +0800

    generate text embedding

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -6,7 +6,23 @@ def verify_model() -> None:
   print(f"Model Loaded: {semantic_search.model}")
   print(f"Max sequence length: {semantic_search.model.max_seq_length}")
 
+def embed_text(text: str) -> None:
+  semantic_search = SemanticSearch()
+  embedding = semantic_search.generate_embedding(text)
+  print(f"Text: {text}")
+  print(f"First 3 dimensions: {embedding[:3]}")
+  print(f"Dimensions: {embedding.shape[0]}")
+
 class SemanticSearch:
     def __init__(self) -> None:
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
         
+    def generate_embedding(self, text: str):
+      # if input text is empty or contains only whitespace, raise a ValueError
+      
+      if not text or not text.strip():
+        raise ValueError("Input text cannot be empty or contain only whitespace")
+      
+      # generate and return the embedding
+      embeddings = self.model.encode([text])
+      return embeddings[0]
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
+
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
-    
+
     subparsers.add_parser("verify", help="Verify the semantic search model")
-    
+
+    embed_parser = subparsers.add_parser(
+        "embed_text", help="Generate embeddings for input text"
+    )
+    embed_parser.add_argument("text", type=str, help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
+
 
 if __name__ == "__main__":
     main()
