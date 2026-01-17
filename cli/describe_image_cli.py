6d7a8b5d429efe3b8bd2aec7196a18ca44b974d3
Greg Delgado <grd888@gmail.com>
2026-01-18 06:51:09 +0800

    rewriting a query from image to text

---
+++ cli/describe_image_cli.py
@@ -0,0 +1,13 @@
+import argparse
+from lib.multimodal_search import describe_command
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Multimodal Search CLI")
+    parser.add_argument("--image", type=str, help="Path of image to describe")
+    parser.add_argument("--query", type=str, help="Query to use for description")
+
+    args = parser.parse_args()
+    result = describe_command(args.image, args.query)
+
+if __name__ == "__main__":
+    main()
