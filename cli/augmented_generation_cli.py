3def2c1b98f0071fda3897a6eae408f0bfd66935
Greg Delgado <grd888@gmail.com>
2026-01-17 16:00:21 +0800

    LLM Sumarization

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.generation import rag_command
+from lib.generation import rag_command, summarize_command
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
@@ -9,13 +9,21 @@ def main():
         "rag", help="Perform RAG (search + generate answer)"
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
-
+    summarize_parser = subparsers.add_parser(
+        "summarize", help="Summarize command"
+    )
+    summarize_parser.add_argument("query", type=str, help="Search query for RAG")
+    summarize_parser.add_argument("--limit", type=int, default=5, help="Limit")
     args = parser.parse_args()
 
     match args.command:
         case "rag":
             query = args.query
             rag_command(query)
+        case "summarize":
+            query = args.query
+            limit = args.limit
+            summarize_command(query, limit)
         case _:
             parser.print_help()
 
--- cli/lib/generation.py
+++ cli/lib/generation.py
@@ -21,7 +21,20 @@ Documents:
 {chr(10).join(docs)}
 
 Provide a comprehensive answer that addresses the query:"""
-
+    response = client.models.generate_content(model=model, contents=prompt)
+    return response.text
+ 
+  
+def summarize_results(query: str, results: list[dict]):
+    prompt = f"""Provide information useful to this query by synthesizing information from multiple search results in detail.
+The goal is to provide comprehensive information so that users know what their options are.
+Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
+This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+Query: {query}
+Search Results:
+{chr(10).join(results)}
+Provide a comprehensive 3–4 sentence answer that combines information from multiple sources:
+"""
     response = client.models.generate_content(model=model, contents=prompt)
     return response.text
 
@@ -32,10 +45,25 @@ def rag_command(query: str) -> dict:
     search_results = hybrid_search.rrf_search(query, k=60, limit=5)
     docs = [f"{doc['title']} - {doc['document']}" for doc in search_results]
     answer = generate_answer(query, docs)
-    
+
     print("Search Results:")
     for res in search_results:
         print(f"- {res['title']}")
     print()
     print("RAG Response:")
     print(answer)
+
+
+def summarize_command(query: str, limit: int = 5) -> dict:
+    movies = load_movies()
+    hybrid_search = HybridSearch(movies)
+    search_results = hybrid_search.rrf_search(query, k=60, limit=limit)
+    docs = [f"{doc['title']} - {doc['document']}" for doc in search_results]
+    summary = summarize_results(query, docs)
+    print("Search Results:")
+    for res in search_results:
+        print(f"- {res['title']}")
+    print()
+    print("LLM Summary:")
+    print(summary)
+    
